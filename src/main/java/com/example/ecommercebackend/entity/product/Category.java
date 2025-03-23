b7906293351b5e2612f230cabd4145b9610a3361
Fatih <fatih_0226@outlook.com>
2025-03-23 20:21:55 +0300

    category kısmen bitmiş bulunmakta product eklendikten sonra category de eklenecektir

--- src/main/java/com/example/ecommercebackend/entity/product/Category.java
+++ src/main/java/com/example/ecommercebackend/entity/product/Category.java
@@ -1,9 +1,151 @@
 package com.example.ecommercebackend.entity.product;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.ecommercebackend.entity.user.Admin;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
-@Table(name = "category")
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_seq")
+    @SequenceGenerator(name = "category_seq", sequenceName = "category_seq", allocationSize = 1)
+    private int id;
+
+    @Column(name = "category_name", nullable = false, unique = true)
+    private String categoryName;
+
+    @Column(name = "category_description")
+    private String categoryDescription;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active = true;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = java.time.LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt = java.time.LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", referencedColumnName = "id")
+    private Admin createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "updated_by", referencedColumnName = "id")
+    private Admin updatedBy;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id", referencedColumnName = "id")
+    private Category parentCategory;
+
+    @OneToMany(mappedBy = "parentCategory", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Category> subCategories = new HashSet<>();
+
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT TRUE")
+    private boolean isSubCategory = true;
+
+    public Category(String categoryName, String categoryDescription, Boolean active, Admin createdBy, Admin updatedBy, Category parentCategory) {
+        this.categoryName = categoryName;
+        this.categoryDescription = categoryDescription;
+        this.active = active;
+        this.createdBy = createdBy;
+        this.updatedBy = updatedBy;
+        this.parentCategory = parentCategory;
+    }
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Admin getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Admin createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Admin getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Admin updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Category getParentCategory() {
+        return parentCategory;
+    }
+
+    public void setParentCategory(Category parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+
+    public Set<Category> getSubCategories() {
+        return subCategories;
+    }
+
+    public void setSubCategories(Set<Category> subCategories) {
+        this.subCategories = subCategories;
+    }
+
+    public boolean isSubCategory() {
+        return isSubCategory;
+    }
+
+    public void setSubCategory(boolean subCategory) {
+        isSubCategory = subCategory;
+    }
 }
