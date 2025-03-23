7d5198bfd91e8d3c75f56964694c975d7ef571af
Fatih <fatih_0226@outlook.com>
2025-03-23 20:01:36 +0300

    file sınıfı direk kopyalandı düzenlemeler yapılacaktır

---
+++ src/main/java/com/example/ecommercebackend/entity/product/Category.java
@@ -0,0 +1,9 @@
+package com.example.ecommercebackend.entity.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+}
