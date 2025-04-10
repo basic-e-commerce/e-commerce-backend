0431c91589bb7ba2009949effff57444adf2a86b
Fatih <fatih_0226@outlook.com>
2025-04-10 22:23:05 +0300

    product small dto yu oluşturacağım yarım kaldı acil olarak refresh tokeni oluşturacağım

---
+++ src/main/java/com/example/ecommercebackend/dto/product/products/ProductSmallDto.java
@@ -0,0 +1,12 @@
+package com.example.ecommercebackend.dto.product.products;
+
+import java.math.BigDecimal;
+
+public class ProductSmallDto {
+    private int id;
+    private String name;
+    private String productLinkName;
+    private BigDecimal salePrice;
+    private BigDecimal comparePrice;
+
+}
