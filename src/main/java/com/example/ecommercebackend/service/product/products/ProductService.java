db96fddc89a64e76ec2f8682507f65c560936dea
Fatih <fatih_0226@outlook.com>
2025-05-24 19:27:23 +0300

    product update güncellendi

--- src/main/java/com/example/ecommercebackend/service/product/products/ProductService.java
+++ src/main/java/com/example/ecommercebackend/service/product/products/ProductService.java
@@ -118,49 +118,49 @@ public class ProductService {
 
 
     public ProductAdminDetailDto updateSimpleProduct(@NotNullParam Integer productId,@NotNullParam ProductUpdateDto productUpdateDto){
-        System.out.println(1);
 
         if (productUpdateDto.getName() == null || productUpdateDto.getName().isEmpty())
             throw new BadRequestException("Product name cannot be empty");
-        System.out.println(2);
 
         if (productUpdateDto.getShortDescription() == null || productUpdateDto.getShortDescription().length() > 165)
             throw new BadRequestException("ShortDescription cannot be longer than 165 characters");
-        System.out.println(3);
+
         if (productUpdateDto.getDescription() == null || productUpdateDto.getDescription().isEmpty())
             throw new BadRequestException("Product description cannot be empty");
-        System.out.println(4);
+
         if ((productUpdateDto.getComparePrice() == null)
                 || (productUpdateDto.getComparePrice().doubleValue() < 0)
                 || productUpdateDto.getComparePrice().compareTo(productUpdateDto.getSalePrice()) > 0){
             throw new BadRequestException("ComparePrice cannot be greater than SalePrice");
         }
-        System.out.println(5);
 
         if (productUpdateDto.getQuantity() < 0)
             throw new BadRequestException("Quantity cannot be less than 0");
-        System.out.println(6);
+
         if (productUpdateDto.getTaxRate().compareTo(BigDecimal.valueOf(100)) > 0 || productUpdateDto.getTaxRate().compareTo(BigDecimal.ZERO) < 0)
             throw new BadRequestException("Tax rate must be between 0 and 100");
-        System.out.println(7);
-        if (productRepository.existsByProductNameEqualsIgnoreCase(productUpdateDto.getName()))
-            throw new ResourceAlreadyExistException("Product name already exists");
-
-        System.out.println(8);
 
         Product product = findProductById(productId);
-        System.out.println(9);
+
+        if(!product.getProductName().equals(productUpdateDto.getName())){
+            if (productRepository.existsByProductNameEqualsIgnoreCase(productUpdateDto.getName()))
+                throw new ResourceAlreadyExistException("Product name already exists");
+        }
+
+
+
+
+
         // Mevcut kategorileri al
         Set<Integer> existCategoryIds = product.getCategories().stream().map(Category::getId).collect(Collectors.toSet());
         Set<Category> existCategory = new HashSet<>(product.getCategories());
-        System.out.println(10);
+
         // Yeni gelen kategori ID'lerini al
         Set<Integer> newCategoryIds = new HashSet<>(productUpdateDto.getCategoryIds());
-        System.out.println(11);
+
         // Yeni eklenmesi gereken kategorileri bul
         Set<Category> categoriesToAdd = new HashSet<>();
         for (Integer categoryId : newCategoryIds) {
-            System.out.println(12);
             if (!existCategoryIds.contains(categoryId)){
                 Category category = categoryService.findCategoryById(categoryId);
                 if (category.isSubCategory()){
@@ -170,43 +170,28 @@ public class ProductService {
                 }
             }
         }
-        System.out.println(13);
+
         // Çıkarılması gereken kategorileri bul
         Set<Category> categoriesToRemove = existCategory.stream()
                 .filter(c -> !newCategoryIds.contains(c.getId()))
                 .collect(Collectors.toSet());
 
-        System.out.println(14);
+
         product.getCategories().removeAll(categoriesToRemove);
-        System.out.println(15);
         product.getCategories().addAll(categoriesToAdd);
-        System.out.println(16);
         product.setProductName(productUpdateDto.getName());
-        System.out.println(17);
         product.setProductLinkName(generateLinkName(product.getProductName()));
-        System.out.println(18);
         product.setSalePrice(productUpdateDto.getSalePrice());
-        System.out.println(19);
         product.setComparePrice(productUpdateDto.getComparePrice());
-        System.out.println(20);
         product.setBuyingPrice(productUpdateDto.getBuyingPrice());
-        System.out.println(21);
         product.setQuantity(productUpdateDto.getQuantity());
-        System.out.println(22);
         product.setShortDescription(productUpdateDto.getShortDescription());
-        System.out.println(23);
         product.setProductDescription(productUpdateDto.getDescription());
-        System.out.println(24);
         ProductType productType = ProductType.valueOf(productUpdateDto.getProductType());
-        System.out.println(25);
         product.setProductType(productType);
-        System.out.println(26);
         product.setPublished(productUpdateDto.getPublished());
-        System.out.println(27);
         product.setDisableOutOfStock(productUpdateDto.getDisableOutOfStock());
-        System.out.println(28);
         product.setTaxRate(productUpdateDto.getTaxRate());
-        System.out.println(29);
 
         return productBuilder.productToProductAdmindetailDto(productRepository.save(product)); // Değişiklik yoksa, gereksiz `save` çağrısı yapma
 
