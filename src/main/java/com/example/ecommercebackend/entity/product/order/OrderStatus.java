4c9cd91c85cb9710c532de4a02d2adb9846ea691
Fatih <fatih_0226@outlook.com>
2025-07-21 21:48:50 +0300

    asd

--- src/main/java/com/example/ecommercebackend/entity/product/order/OrderStatus.java
+++ src/main/java/com/example/ecommercebackend/entity/product/order/OrderStatus.java
@@ -20,7 +20,7 @@ public class OrderStatus {
     @Enumerated(EnumType.STRING)
     private Status status = Status.PENDING;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @OneToMany(cascade = CascadeType.ALL,orphanRemoval = true)
     private List<OrderPackage> orderPackages = new ArrayList<>();
 
     @Column(name = "receipt_address_id")
