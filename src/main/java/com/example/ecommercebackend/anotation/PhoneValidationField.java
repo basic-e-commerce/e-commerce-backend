c15c72342f603cb1a1e3cc44950f7aaf8b3a8b8e
Fatih <fatih_0226@outlook.com>
2025-08-06 13:56:05 +0300

    phonevalidation güncellendi

--- src/main/java/com/example/ecommercebackend/anotation/PhoneValidationField.java
+++ src/main/java/com/example/ecommercebackend/anotation/PhoneValidationField.java
@@ -7,13 +7,13 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.regex.Pattern;
 
 @Aspect
 @Component
 public class PhoneValidationField {
     private static final Pattern PHONE_PATTERN = Pattern.compile("^\\+90\\d{10}$");
-
     @Before("execution(* com.example.ecommercebackend.controller..*(..))")
     public void validateNotNullFields(JoinPoint joinPoint) throws IllegalAccessException {
         for (Object arg : joinPoint.getArgs()) {
@@ -21,10 +21,15 @@ public class PhoneValidationField {
 
             Field[] fields = arg.getClass().getDeclaredFields();
             for (Field field : fields) {
+                int modifiers = field.getModifiers();
+                if (Modifier.isStatic(modifiers) || Modifier.isFinal(modifiers)) {
+                    // static veya final alanları atla
+                    continue;
+                }
+
                 field.setAccessible(true);
                 Object value = field.get(arg);
 
-                // Telefon numarası kontrolü: null değilse kontrol et
                 if (field.isAnnotationPresent(ValidPhoneNumber.class) && value != null) {
                     String phone = value.toString();
                     if (!PHONE_PATTERN.matcher(phone).matches()) {
@@ -36,4 +41,5 @@ public class PhoneValidationField {
             }
         }
     }
+
 }
